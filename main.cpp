b7a2e4f01ab41cae211edb02d7288863652c5cac
Rudy Bunel <bunel.rudy@gmail.com>
2016-06-07 10:46:09 +0100

    Initial commit with project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "optim.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::cout << square(2) << '\n';
+    return 0;
+}
---
+++ sfm-min-lib/optim.cpp
@@ -0,0 +1,4 @@
+
+double square(double nb){
+    return nb * nb;
+}
